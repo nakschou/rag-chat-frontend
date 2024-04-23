079eb849dde0528668f398345adce4a00382daa0
Justin Chou <87676813+nakschou@users.noreply.github.com>
2024-04-23 02:57:38 -0700

    reformatted

--- src/components/chatroom.tsx
+++ src/components/chatroom.tsx
@@ -1,12 +1,10 @@
-'use client'
-import { useRef, useEffect, useState } from 'react';
-import { Button } from "@/components/ui/button"
-import { Textarea } from "@/components/ui/textarea"
+"use client";
+import { useRef, useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
 import Link from "next/link";
-import { Skeleton } from "@/components/ui/skeleton"
-import { useRouter } from 'next/navigation';
-import { CaretSortIcon, CheckIcon } from "@radix-ui/react-icons"
-import { cn } from "@/lib/utils"
+import { Skeleton } from "@/components/ui/skeleton";
+import { useRouter } from "next/navigation";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -14,127 +12,122 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-
-
+} from "@/components/ui/dropdown-menu";
 
 export default function ChatRoom({ params }) {
-  
-    const router = useRouter();
+  const router = useRouter();
 
-    const url_start = process.env.NEXT_PUBLIC_MY_API_ENDPOINT;
-    // State to hold messages
-    const [messages, setMessages] = useState([]);
-    const [isLoadingMessage, setIsLoadingMessage] = useState(false); // State to hold loading status
-    const messagesEndRef = useRef(null); // Create a ref for the messages container
-    const [value, setValue] = useState('')
+  const url_start = process.env.NEXT_PUBLIC_MY_API_ENDPOINT;
+  // State to hold messages
+  const [messages, setMessages] = useState([]);
+  const [isLoadingMessage, setIsLoadingMessage] = useState(false); // State to hold loading status
+  const messagesEndRef = useRef(null); // Create a ref for the messages container
+  const [value, setValue] = useState("");
 
-    // Scroll to the bottom function
-    const scrollToBottom = () => {
-      messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
-    }
-  
-    // Scroll to bottom on initial load and on message updates
-    useEffect(() => {
-      scrollToBottom();
-    }, [messages]); // Assuming 'messages' is your state or prop that updates when a new message is added
-    
-    const [newMessage, setNewMessage] = useState('');
-    const id = params.id;
-    
+  // Scroll to the bottom function
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
 
-    // function to fetch messages
-    const fetchMessages = () => {
-        //check id exists first
-        fetch(`${url_start}confirm_id?id=${id}`, {
-          method: 'GET'
-        })
-          .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-          if (data.exists == false) {
-            router.push('/chat/invalid-id');  // Navigate to /chat/invalid-id on error
-          }
-        })
-        fetch(`${url_start}get_messages?id=${id}`, {
-          method: 'GET'
-        })
-          .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-          //convert data.messages strings to objects
-          data.messages = data.messages.map((message) => {
-            return JSON.parse(message);
-          });
-          setMessages(data.messages);
-        })
-        .catch(error => {
-          console.error('Error fetching messages:', error);
+  // Scroll to bottom on initial load and on message updates
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]); // Assuming 'messages' is your state or prop that updates when a new message is added
+
+  const [newMessage, setNewMessage] = useState("");
+  const id = params.id;
+
+  // function to fetch messages
+  const fetchMessages = () => {
+    //check id exists first
+    fetch(`${url_start}confirm_id?id=${id}`, {
+      method: "GET",
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data.exists == false) {
+          router.push("/chat/invalid-id"); // Navigate to /chat/invalid-id on error
+        }
+      });
+    fetch(`${url_start}get_messages?id=${id}`, {
+      method: "GET",
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        //convert data.messages strings to objects
+        data.messages = data.messages.map((message) => {
+          return JSON.parse(message);
         });
-    };
+        setMessages(data.messages);
+      })
+      .catch((error) => {
+        console.error("Error fetching messages:", error);
+      });
+  };
 
-    // Fetch messages and scroll to bottom
-    useEffect(() => {
-      fetchMessages();
-      scrollToBottom();
-    }, []);
+  // Fetch messages and scroll to bottom
+  useEffect(() => {
+    fetchMessages();
+    scrollToBottom();
+  }, []);
 
-        // Function to handle message submission
-        const handleSubmit = (event) => {
-          event.preventDefault(); // Prevent default form submission behavior
-          setNewMessage(''); // Clear the message input after sending
-        
-          // Start the first fetch operation
-          fetch(url_start + "update_redis", {
-            method: 'POST',
-            mode: 'cors',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ message: newMessage, user: true, id: id }),
-          })
-          .then(response => {
-            if (!response.ok) {
-              throw new Error('Network response was not ok');
-            }
-            fetchMessages(); // Fetch the latest messages after submitting
-            setIsLoadingMessage(true); // Set loading status to true
-            return response; // Pass the response to the next then block if needed
-          })
-          .then(() => {
-            // Start the second fetch operation inside the then block of the first fetch
-            return fetch(url_start + "rag_qa", {
-              mode: 'cors',
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({"query": newMessage, "id": id, "voice": value}),
-            });
-          })
-          .then(response => {
-            console.log(response)
-            if (!response.ok) {
-              throw new Error('Network response was not ok');
-            }
-            fetchMessages(); // Fetch the latest messages again if needed
-          })
-          .catch(error => {
-            console.error('Error sending message:', error);
-          })
-          .finally(() => {
-            setIsLoadingMessage(false); // Set loading status to false at the end
-          });
-        };
-        
+  // Function to handle message submission
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevent default form submission behavior
+    setNewMessage(""); // Clear the message input after sending
+
+    // Start the first fetch operation
+    fetch(url_start + "update_redis", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message: newMessage, user: true, id: id }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        fetchMessages(); // Fetch the latest messages after submitting
+        setIsLoadingMessage(true); // Set loading status to true
+        return response; // Pass the response to the next then block if needed
+      })
+      .then(() => {
+        // Start the second fetch operation inside the then block of the first fetch
+        return fetch(url_start + "rag_qa", {
+          mode: "cors",
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ query: newMessage, id: id, voice: value }),
+        });
+      })
+      .then((response) => {
+        console.log(response);
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        fetchMessages(); // Fetch the latest messages again if needed
+      })
+      .catch((error) => {
+        console.error("Error sending message:", error);
+      })
+      .finally(() => {
+        setIsLoadingMessage(false); // Set loading status to false at the end
+      });
+  };
 
   return (
     <main className="h-screen w-screen flex items-center justify-center">
@@ -142,66 +135,97 @@ export default function ChatRoom({ params }) {
         <div className="flex-1 border-b">
           <div className="p-4 flex flex-col gap-2">
             <div className="flex justify-between items-center w-full">
-            <Link
-              className="flex items-center rounded-lg bg-gray-100 px-3 py-2 text-lg font-medium dark:bg-gray-800"
-              href="/"
-            >
-              <HomeIcon className="h-6 w-6" />
-            </Link>
-            <div className="text-sm font-medium text-gray-500 dark:text-gray-400">Chatroom ID: {id}</div>
-            <DropdownMenu className="justify-end">
-              <DropdownMenuTrigger className="bg-gray-100 px-3 rounded-lg justify-end">{value? value : "Voices"}</DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuLabel>Try filters</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem onClick={() => setValue("")}>Default</DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setValue("Cowboy")}>Cowboy 🤠</DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setValue("Shakespeare")}>Shakespeare 🎭</DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setValue("Surfer")}>Surfer 🏄‍♂️</DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setValue("Gen Z")}>Gen Z 🤳</DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setValue("Pirate")}>Pirate 🏴‍☠️</DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
+              <Link
+                className="flex items-center rounded-lg bg-gray-100 px-3 py-2 text-lg font-medium dark:bg-gray-800"
+                href="/"
+              >
+                <HomeIcon className="h-6 w-6" />
+              </Link>
+              <div className="text-sm font-medium text-gray-500 dark:text-gray-400">
+                Chatroom ID: {id}
+              </div>
+              <DropdownMenu className="justify-end">
+                <DropdownMenuTrigger className="bg-gray-100 px-3 rounded-lg justify-end">
+                  {value ? value : "Voices"}
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuLabel>Try filters</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem onClick={() => setValue("")}>
+                    Default
+                  </DropdownMenuItem>
+                  <DropdownMenuItem onClick={() => setValue("Cowboy")}>
+                    Cowboy 🤠
+                  </DropdownMenuItem>
+                  <DropdownMenuItem onClick={() => setValue("Shakespeare")}>
+                    Shakespeare 🎭
+                  </DropdownMenuItem>
+                  <DropdownMenuItem onClick={() => setValue("Surfer")}>
+                    Surfer 🏄‍♂️
+                  </DropdownMenuItem>
+                  <DropdownMenuItem onClick={() => setValue("Gen Z")}>
+                    Gen Z 🤳
+                  </DropdownMenuItem>
+                  <DropdownMenuItem onClick={() => setValue("Pirate")}>
+                    Pirate 🏴‍☠️
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
             </div>
             <div className="p-4 flex flex-col gap-2">
               {Object.keys(messages).map((messageId) => {
                 const message = messages[messageId];
                 const user = message.user;
                 return (
-                  <div key={messageId} className={`flex items-center gap-2 justify-start`}>
+                  <div
+                    key={messageId}
+                    className={`flex items-center gap-2 justify-start`}
+                  >
                     <div className="rounded-full overflow-hidden border-2 border-white w-10 h-10">
                       <img
                         alt={`${message.createdBy} avatar`}
                         className="object-cover w-full h-full"
-                        src={message.avatarUrl || user ? "https://static.vecteezy.com/system/resources/thumbnails/008/442/086/small_2x/illustration-of-human-icon-user-symbol-icon-modern-design-on-blank-background-free-vector.jpg": "https://pbs.twimg.com/profile_images/1719004662630580224/B9j_z9iG_400x400.jpg"}
+                        src={
+                          message.avatarUrl || user
+                            ? "https://static.vecteezy.com/system/resources/thumbnails/008/442/086/small_2x/illustration-of-human-icon-user-symbol-icon-modern-design-on-blank-background-free-vector.jpg"
+                            : "https://pbs.twimg.com/profile_images/1719004662630580224/B9j_z9iG_400x400.jpg"
+                        }
                       />
                     </div>
-                    <div className={`w-full rounded-lg ${user ? 'bg-blue-100 dark:bg-blue-800' : 'bg-gray-100 dark:bg-gray-800'}`}>
+                    <div
+                      className={`w-full rounded-lg ${
+                        user
+                          ? "bg-blue-100 dark:bg-blue-800"
+                          : "bg-gray-100 dark:bg-gray-800"
+                      }`}
+                    >
                       <div className="p-4 text-sm">{message.text}</div>
                     </div>
                   </div>
                 );
               })}
               {isLoadingMessage && (
-              <div className="flex items-center gap-2 justify-start">
-                    <div className="rounded-full overflow-hidden border-2 border-white w-10 h-10">
-                      <img
-                        alt={`avatar`}
-                        className="object-cover w-full h-full"
-                        src={"https://pbs.twimg.com/profile_images/1719004662630580224/B9j_z9iG_400x400.jpg"}
-                      />
+                <div className="flex items-center gap-2 justify-start">
+                  <div className="rounded-full overflow-hidden border-2 border-white w-10 h-10">
+                    <img
+                      alt={`avatar`}
+                      className="object-cover w-full h-full"
+                      src={
+                        "https://pbs.twimg.com/profile_images/1719004662630580224/B9j_z9iG_400x400.jpg"
+                      }
+                    />
+                  </div>
+                  <div className="w-full rounded-lg bg-gray-100 dark:bg-gray-800">
+                    <div className="p-4 text-sm">
+                      <Skeleton className="w-full h-[20px] rounded-full" />
+                      <div className="h-2" />
+                      <Skeleton className="w-full h-[20px] rounded-full" />
+                      <div className="h-2" />
+                      <Skeleton className="w-full h-[20px] rounded-full" />
                     </div>
-                    <div className="w-full rounded-lg bg-gray-100 dark:bg-gray-800">
-                      <div className="p-4 text-sm">
-                        <Skeleton className="w-full h-[20px] rounded-full" />
-                        <div className="h-2" />
-                        <Skeleton className="w-full h-[20px] rounded-full" />
-                        <div className="h-2" />
-                        <Skeleton className="w-full h-[20px] rounded-full" />
-                      </div>
-                    </div>
-              </div>
-            )}
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         </div>
@@ -209,18 +233,18 @@ export default function ChatRoom({ params }) {
           <div className="w-full max-w-[600px]">
             <form className="flex gap-4" onSubmit={handleSubmit}>
               <Textarea
-                  className="flex-1 min-h-[100px]"
-                  placeholder="Type your message..."
-                  value={newMessage}
-                  onChange={(e) => setNewMessage(e.target.value)}
-                />
+                className="flex-1 min-h-[100px]"
+                placeholder="Type your message..."
+                value={newMessage}
+                onChange={(e) => setNewMessage(e.target.value)}
+              />
               <Button type="submit">Send</Button>
             </form>
           </div>
         </div>
       </div>
     </main>
-  )
+  );
 }
 
 function HomeIcon(props) {
@@ -240,5 +264,5 @@ function HomeIcon(props) {
       <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
       <polyline points="9 22 9 12 15 12 15 22" />
     </svg>
-  )
+  );
 }
--- src/components/homepage.tsx
+++ src/components/homepage.tsx
@@ -1,88 +1,88 @@
-'use client'
-import React, { useState } from 'react';
-import { Input } from "@/components/ui/input"
-import { Button } from './ui/button';
+"use client";
+import React, { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "./ui/button";
 import { useRouter } from "next/navigation";
-import { Progress } from './ui/progress';
+import { Progress } from "./ui/progress";
 
 export default function HomePage() {
-    const router = useRouter(); 
-    const [file, setFile] = useState(null);
-    const [isLoading, setIsLoading] = useState(false);
-    const [sessionId, setSessionId] = useState('');
-    const [id, setId] = useState(''); // Add this line
-    const [progress, setProgress] = React.useState(13)
+  const router = useRouter();
+  const [file, setFile] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [sessionId, setSessionId] = useState("");
+  const [id, setId] = useState(""); // Add this line
+  const [progress, setProgress] = React.useState(13);
 
-    const url_start = process.env.NEXT_PUBLIC_MY_API_ENDPOINT;
+  const url_start = process.env.NEXT_PUBLIC_MY_API_ENDPOINT;
 
-    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        if (event.target.files && event.target.files[0]) {
-          setFile(event.target.files[0]); // Set the selected file
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files[0]) {
+      setFile(event.target.files[0]); // Set the selected file
+    }
+  };
+
+  const handleSubmit = async () => {
+    if (!file) {
+      alert("Please select a file first!");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", file);
+    setIsLoading(true);
+
+    fetch(`${url_start}generate_id`, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
         }
-      };
-
-      const handleSubmit = async () => {
-        if (!file) {
-          alert('Please select a file first!');
-          return;
-        }
-    
-        const formData = new FormData();
-        formData.append('file', file);
-        setIsLoading(true);
-    
-        fetch(`${url_start}generate_id`, {
-          method: 'POST',
-          mode: 'cors',
-          headers: {
-            'Content-Type': 'application/json',
-          },
+        return response.json();
+      })
+      .then((data) => {
+        formData.append("id", data.id);
+        setId(data.id);
+        fetch(`${url_start}pdf_to_pinecone`, {
+          method: "POST",
+          mode: "cors",
+          body: formData,
         })
-        .then(response => {
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          return response.json();
-        })
-        .then(data => {
-          formData.append('id', data.id);
-          setId(data.id);
-          fetch(`${url_start}pdf_to_pinecone`, {
-            method: 'POST',
-            mode: 'cors',
-            body: formData,
-          }).then(response => {
-              console.log(response)
-              if (!response.ok) {
-                throw new Error('Network response was not ok');
-              }
-              return response.json();
-            })
-            .then(() => {
-              router.push(`/chat/${data.id}`);
-            })
-            .catch(error => {
-              setIsLoading(false);
-              console.error('There was an error!', error);
-            });
+          .then((response) => {
+            console.log(response);
+            if (!response.ok) {
+              throw new Error("Network response was not ok");
+            }
+            return response.json();
           })
-        };
+          .then(() => {
+            router.push(`/chat/${data.id}`);
+          })
+          .catch((error) => {
+            setIsLoading(false);
+            console.error("There was an error!", error);
+          });
+      });
+  };
 
+  const handleSessionSubmit = async () => {
+    if (!sessionId) {
+      alert("Please enter a session ID!");
+      return;
+    }
+    router.push(`/chat/${sessionId}`);
+  };
 
-      const handleSessionSubmit = async () => {
-        if (!sessionId) {
-            alert('Please enter a session ID!');
-            return;
-        }
-        router.push(`/chat/${sessionId}`);
-      };
+  React.useEffect(() => {
+    const timer = setTimeout(() => setProgress(66), 10000);
+    return () => clearTimeout(timer);
+  }, [isLoading]);
 
-      React.useEffect(() => {
-        const timer = setTimeout(() => setProgress(66), 10000)
-        return () => clearTimeout(timer)
-      }, [isLoading])
-
-  if(isLoading) {
+  if (isLoading) {
     return (
       <div className="flex flex-col items-center justify-center h-screen space-y-8">
         <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl">
@@ -101,7 +101,7 @@ export default function HomePage() {
         <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 bg-gray-100 dark:bg-gray-900">
           <div className="container px-4 md:px-6 flex flex-col items-center text-center space-y-6">
             <div className="space-y-4">
-            <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl">
+              <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl">
                 PDF Q&A
               </h1>
               <p className="max-w-[700px] text-gray-600 dark:text-gray-400 md:text-xl">
@@ -120,22 +120,24 @@ export default function HomePage() {
               </Button>
             </div>
             <div className="w-full max-w-md flex items-center">
-            <Input
+              <Input
                 className="flex-1"
                 placeholder="Or, enter your session ID here"
                 value={sessionId}
                 onChange={(e) => setSessionId(e.target.value)}
-            />
-            <Button onClick={handleSessionSubmit} className="ml-4">
+              />
+              <Button onClick={handleSessionSubmit} className="ml-4">
                 Submit Session ID
-            </Button>
+              </Button>
             </div>
           </div>
         </section>
         <section className="w-full py-12 md:py-24 lg:py-32">
           <div className="container px-4 md:px-6 grid gap-12 lg:grid-cols-2">
             <div className="space-y-4 p-12">
-              <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">How to Use PDF Q&A</h2>
+              <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
+                How to Use PDF Q&A
+              </h2>
               <p className="text-gray-600 dark:text-gray-400 md:text-xl">
                 Follow these steps to get started.
               </p>
@@ -148,7 +150,10 @@ export default function HomePage() {
                 <div>
                   <h3 className="font-semibold">Choose your PDF</h3>
                   <p className="text-gray-600 dark:text-gray-400">
-                    Click the upload button above and select your file. If you have a previous session saved, you may use that in the box below. Or, type default for the default PDF (Startup Playbook). 
+                    Click the upload button above and select your file. If you
+                    have a previous session saved, you may use that in the box
+                    below. Or, type default for the default PDF (Startup
+                    Playbook).
                   </p>
                 </div>
               </div>
@@ -159,7 +164,8 @@ export default function HomePage() {
                 <div>
                   <h3 className="font-semibold">Wait for preparation</h3>
                   <p className="text-gray-600 dark:text-gray-400">
-                  PDF Q&A needs time to process your PDF. Please be patient as it prepares the document for you.
+                    PDF Q&A needs time to process your PDF. Please be patient as
+                    it prepares the document for you.
                   </p>
                 </div>
               </div>
@@ -170,7 +176,8 @@ export default function HomePage() {
                 <div>
                   <h3 className="font-semibold">Chat with your PDF</h3>
                   <p className="text-gray-600 dark:text-gray-400">
-                    You can now chat with your PDF. Type your message in the box and press enter to send it.
+                    You can now chat with your PDF. Type your message in the box
+                    and press enter to send it.
                   </p>
                 </div>
               </div>
@@ -178,11 +185,13 @@ export default function HomePage() {
           </div>
         </section>
       </main>
-      <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t"> 
+      <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
-            <p className="text-xs text-gray-500 dark:text-gray-400">Created by Justin Chou for Infinitus AI</p>
+          <p className="text-xs text-gray-500 dark:text-gray-400">
+            Created by Justin Chou for Infinitus AI
+          </p>
         </nav>
       </footer>
     </div>
-  )
+  );
 }
